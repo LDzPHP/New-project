42bf95d5a7ab5a5e1d76dd44f0c6407f73467807
ldz <ldz@DELLi5.localdomain>
2022-05-08 13:45:46 +0300

    Add new controller and route to it

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pages', [PageController::class, 'pages']);
 
 Route::get('/hello.blade', function () {
     return view('hello', ['name' => 'Linda']);
